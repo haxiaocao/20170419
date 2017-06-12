71fd7eed5aec0a52d28163bc5fe14fc3ff26e166
ixaocao <guokai2007@163.com>
2017-06-12 21:12:48 +0800

    add the solution to the remote repository

---
+++ Test/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Person
+    {
+        public int Age { get;private set; }
+        public string Name { get;private set; }
+        public static readonly string TO_STRING = "OK,101";
+
+        List<Person> friends = new List<Person>();
+        public List<Person> Friends
+        {
+            get { return friends; }
+        }
+
+        //Location home = new Location();
+        //public Location Home
+        //{
+        //    get { return home; }
+        //}
+
+        public Person()
+        {
+            Console.WriteLine("No argument constructor:");
+            Console.WriteLine("readonly tosting:"+TO_STRING);
+        }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public Person(string name,int age)
+        {
+            Name = name;
+            Age = age;
+            Console.WriteLine("readonly tostring :"+TO_STRING);
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void PrintType<T>(T first,T second)
+        {
+            Console.WriteLine(typeof(T));
+        }
+        static void Main(string[] args)
+        {
+            //PrintType(1, new object());
+            //PrintType<object>(1,new object());
+
+            //PrintType(1,2);
+            //PrintType<int>(1,2);
+
+
+
+            Console.ReadLine();
+        }
+    }   
+}
